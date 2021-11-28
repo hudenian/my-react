63a5377ae5d673ba43bc4e32c8d91fd7a31392c9
hudenian <hudenian@163.com>
2021-11-28 15:41:37 +0800

    add login funcion

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -63,6 +63,8 @@ const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
+const lessRegex = /\.less$/;
+const lessModuleRegex = /\.module\.less$/;
 
 const hasJsxRuntime = (() => {
   if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
@@ -97,7 +99,7 @@ module.exports = function (webpackEnv) {
   const shouldUseReactRefresh = env.raw.FAST_REFRESH;
 
   // common function to get style loaders
-  const getStyleLoaders = (cssOptions, preProcessor) => {
+  const getStyleLoaders = (cssOptions,lessOptions, preProcessor) => {
     const loaders = [
       isEnvDevelopment && require.resolve('style-loader'),
       isEnvProduction && {
@@ -111,6 +113,10 @@ module.exports = function (webpackEnv) {
       {
         loader: require.resolve('css-loader'),
         options: cssOptions,
+      },
+      {
+        loader: require.resolve('less-loader'),
+        options: lessOptions,
       },
       {
         // Options for PostCSS as we reference these options twice
@@ -532,6 +538,39 @@ module.exports = function (webpackEnv) {
                   },
                 },
                 'sass-loader'
+              ),
+            },
+            {
+              test: lessRegex,
+              exclude: lessModuleRegex,
+              use: getStyleLoaders({
+                    importLoaders: 1,
+                    sourceMap: isEnvProduction
+                        ? shouldUseSourceMap
+                        : isEnvDevelopment,
+                  },
+                  'less-loader'
+              ),
+              // Don't consider CSS imports dead code even if the
+              // containing package claims to have no side effects.
+              // Remove this when webpack adds a warning or an error for this.
+              // See https://github.com/webpack/webpack/issues/6571
+              sideEffects: true,
+            },
+            // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
+            // using the extension .module.css
+            {
+              test: lessModuleRegex,
+              use: getStyleLoaders({
+                    importLoaders: 1,
+                    sourceMap: isEnvProduction
+                        ? shouldUseSourceMap
+                        : isEnvDevelopment,
+                    modules: {
+                      getLocalIdent: getCSSModuleLocalIdent,
+                    },
+                  },
+                  'less-loader'
               ),
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {Button, Form, Input} from 'antd';
+import {LockOutlined, UserOutlined} from '@ant-design/icons';
+import './login.less'
+import bg from './images/bg.jpeg'
+
+const Item = Form.Item
+
+class Login extends Component {
+    render() {
+        const onFinish = e => {
+            //阻止表单提交
+            e.preventDefault();
+            console.log('Received values of form: ');
+        };
+
+        return (
+            <div className="login">
+                <div className="login-header">
+                    <img src={bg} alt="logo"/>
+                    <h1>项目：后台管理系统</h1>
+                </div>
+                <div className="login-content">
+                    <h1>用户登陆</h1>
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        initialValues={{remember: true}}
+                        onFinish={onFinish}
+                    >
+                        <Item
+                            name="用户名"
+                            rules={[{required: true, message: '请输入用户名!'}]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="用户名"/>
+                        </Item>
+                        <Item
+                            name="密码"
+                            rules={[{required: true, message: '请输入密码!'}]}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon"/>}
+                                type="password"
+                                placeholder="密码"
+                            />
+                        </Item>
+                        <Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                登录
+                            </Button>
+                        </Item>
+                    </Form>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Login;
