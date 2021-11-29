4c51b41eabd6305d2769f25c779a739d422d3a78
hudenian <hudenian@163.com>
2021-11-29 23:39:31 +0800

    add websocket

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,16 +1,36 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import SockJsClient from "react-stomp";
 
 import "./home.less";
+
+const SOCKET_URL = "http://localhost:8888/notification";
+const GENERAL_TOPIC = "/topic/pubic";
 
 /*
 首页路由组件
 */
-export default class Home extends Component {
-  render() {
-    return (
-      <div className="home">
-        欢迎huma管理系统
-      </div>
-    )
-  }
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    sendMessage = (msg) => {
+        this.clientRef.sendMessage('/sendMessage', msg);  //@注解1
+    }
+
+    render() {
+        return (
+            <div>
+                <SockJsClient url='http://localhost:8888/ws' topics={['/topic/pubic']}
+                              onMessage={(msg) => {
+                                  alert(msg);
+                              }}
+                              ref={(client) => {
+                                  this.clientRef = client
+                              }}/>
+                <div>99999</div>
+            </div>
+        );
+    }
 }
+export default Home
